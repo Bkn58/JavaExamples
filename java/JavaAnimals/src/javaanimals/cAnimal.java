b494e69ba7e406846ab84c1681722bb17c61bd0c
User <User@LAPTOP-Bkn>
2020-09-14 22:23:22 +0600

    Класс cAnimsl вынесен в отдельный файл из класса JavaAnimals

---
+++ java/JavaAnimals/src/javaanimals/cAnimal.java
@@ -0,0 +1,16 @@
+package javaanimals;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Bkn
+ */
+public class cAnimal {
+     ArrayList <String> propAni;   // динамический массив свойств одного животного
+    
+     cAnimal (){
+        propAni = new ArrayList <> (); 
+     }
+     
+}
